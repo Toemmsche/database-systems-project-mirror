69c05f8542654eb77418090126d017822754fd5e
Tom Papke <tom.papke@outlook.de>
2021-12-07 18:48:21 +0100

    [+] Add routes

--- db-frontend/src/app/app-routing.module.ts
+++ db-frontend/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {AppComponent} from "./app.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'Bundestag', component: AppComponent, pathMatch: 'full'},
+  {path: 'Daten', component: AppComponent, pathMatch: 'full'},
+  {path: 'Karte', component: AppComponent, pathMatch: 'full'},
+  {path: 'Statistiken', component: AppComponent, pathMatch: 'full'},
+  {path: '', component: AppComponent, pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
