f38c6e00dabd147d95a4df92871b91fd4a2a3154
Tom Papke <tom.papke@outlook.de>
2021-12-09 18:31:15 +0100

    [~] Make every navigation item redirect to BundestagComponent

--- db-frontend/src/app/app-routing.module.ts
+++ db-frontend/src/app/app-routing.module.ts
@@ -5,9 +5,9 @@ import {BundestagComponent} from "./bundestag/bundestag.component";
 
 const routes: Routes = [
   {path: 'Bundestag', component: BundestagComponent, pathMatch: 'full'},
-  {path: 'Daten', component: AppComponent, pathMatch: 'full'},
-  {path: 'Karte', component: AppComponent, pathMatch: 'full'},
-  {path: 'Statistiken', component: AppComponent, pathMatch: 'full'},
+  {path: 'Daten', component: BundestagComponent, pathMatch: 'full'},
+  {path: 'Karte', component: BundestagComponent, pathMatch: 'full'},
+  {path: 'Statistiken', component: BundestagComponent, pathMatch: 'full'},
   {path: '', component: BundestagComponent, pathMatch: 'full'}
 ];
 
