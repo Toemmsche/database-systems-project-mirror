de60fb0bb6db2cb3ce6714d655d28b28620cadbd
Tom Papke <tom.papke@outlook.de>
2021-12-16 22:33:45 +0100

    [~] Adjust attribute names

--- backend/sql/queries/Begrenzungen.sql
+++ backend/sql/queries/Begrenzungen.sql
@@ -1,5 +1,5 @@
 DROP VIEW IF EXISTS begrenzungen CASCADE;
 
-CREATE VIEW begrenzungen(nummer, name, wahl, begrenzung) AS
+CREATE VIEW begrenzungen(wk_nummer, wk_name, wahl, begrenzung) AS
     SELECT wk.nummer, wk.name, wk.wahl, wk.begrenzung
     FROM wahlkreis wk
