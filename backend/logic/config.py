636a247b9dfdede908d4f3358f98051b4d239eb3
Tom Papke <tom.papke@outlook.de>
2022-01-31 16:01:16 +0100

    use DATABASE_URI from heroku

--- backend/logic/config.py
+++ backend/logic/config.py
@@ -6,4 +6,4 @@ db_config = {
     'password': os.environ.get('POSTGRES_PWD')
 }
 
-conn_string = f"host=localhost port=5432 dbname={db_config['dbname']} user={db_config['user']} password={db_config['password']}" if os.environ.get('DATABASE_URI') is None else s.environ.get('DATABASE_URI')
+conn_string = f"host=localhost port=5432 dbname={db_config['dbname']} user={db_config['user']} password={db_config['password']}" if 'DATABASE_URI' not in os.environ else os.environ.get('DATABASE_URI')
