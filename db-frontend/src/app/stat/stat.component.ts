5d7e0a3aca07bc45ccdedff70110979ffd5a9237
Tom Papke <tom.papke@outlook.de>
2022-01-31 19:22:42 +0100

    [+] Add button for new VergleichsStatistik

--- db-frontend/src/app/stat/stat.component.ts
+++ db-frontend/src/app/stat/stat.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatComponent implements OnInit {
 
+  // Initial component
+  vergleichComponents = [0];
+
   constructor() { }
 
   ngOnInit(): void {
