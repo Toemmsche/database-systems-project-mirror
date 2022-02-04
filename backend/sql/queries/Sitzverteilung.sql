e4d8df3eff9ec8205390364e0a5be31b9b508942
Felix Rinderer <rinderer@in.tum.de>
2022-02-04 13:00:06 +0100

    [+] Show Mandat diff for each party and the Bundestag in landing page

--- backend/sql/queries/Sitzverteilung.sql
+++ backend/sql/queries/Sitzverteilung.sql
@@ -1,9 +1,17 @@
 DROP VIEW IF EXISTS sitzverteilung CASCADE;
 
-CREATE VIEW sitzverteilung(wahl, partei, partei_farbe, sitze) AS
-    SELECT m.wahl, p.kuerzel, p.farbe, COUNT(*)
-    FROM mandat m,
-         partei p
-    WHERE m.partei = p.parteiid
-    GROUP BY m.wahl, p.kuerzel, p.farbe;
+CREATE VIEW sitzverteilung(wahl, partei, partei_farbe, sitze, sitze_rel, sitze_vor, sitze_vor_rel) AS
+    WITH sitzverteilung_ohne_vorperiode(wahl, partei, partei_farbe, sitze, sitze_gesamt) AS (
+        SELECT m.wahl,
+               p.kuerzel,
+               p.farbe,
+               COUNT(*),
+               (SELECT COUNT(*) FROM mandat mg WHERE mg.wahl = m.wahl)
+        FROM mandat m,
+             partei p
+        WHERE m.partei = p.parteiid
+        GROUP BY m.wahl, p.kuerzel, p.farbe)
+
+    SELECT akt.wahl, akt.partei, akt.partei_farbe, akt.sitze, akt.sitze::DECIMAL / akt.sitze_gesamt, vor.sitze, vor.sitze::DECIMAL / vor.sitze_gesamt
+    FROM sitzverteilung_ohne_vorperiode akt FULL OUTER JOIN sitzverteilung_ohne_vorperiode vor ON akt.partei = vor.partei AND akt.wahl = vor.wahl + 1
 
--- frontend/src/model/Sitzverteilung.ts
+++ frontend/src/model/Sitzverteilung.ts
@@ -3,4 +3,7 @@ export class Sitzverteilung {
   partei !: string;
   partei_farbe !: string;
   sitze !: number;
+  sitze_rel !: number;
+  sitze_vor !: number;
+  sitze_vor_rel !: number;
 }
